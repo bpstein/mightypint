7496cab5d92d94542cf757eb648bb5760c47be0c
Ben Stein <bumstain@gmail.com>
2016-01-26 00:44:31 +0000

    Debugged and refactored map script

---
+++ js/map.js
@@ -0,0 +1,68 @@
+eqfeed_callback (
+	{
+	"type": "FeatureCollection",
+		"metadata": {
+			"generated": 1453674469000,
+			"url": "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp",
+			"title": "USGS Magnitude 2.5+ Earthquakes, Past Week",
+			"status": 200,
+			"api": "1.1.1",
+			"count": 291
+		},
+		"features": [
+			
+	{
+	"type": "Feature",
+	"properties": {
+	"mag": 2.5,
+	"place": "84km E of Old Iliamna, Alaska",
+	"time": 1453669081000,
+	"updated": 1453672735143,
+	"tz": -540,
+	"url": "http://earthquake.usgs.gov/earthquakes/eventpage/ak12501966",
+	"detail": "http://earthquake.usgs.gov/earthquakes/feed/v1.0/detail/ak12501966.geojsonp",
+	"felt": 1,
+	"cdi": 3.4,
+	"mmi": null,
+	"alert": null,
+	"status": "automatic",
+	"tsunami": 0,
+	"sig": 96,
+	"net": "ak",
+	"code": "12501966",
+	"ids": ",ak12501966,",
+	"sources": ",ak,",
+	"types": ",dyfi,general-link,geoserve,nearby-cities,origin,tectonic-summary,",
+	"nst": null,
+	"dmin": null,
+	"rms": 0.95,
+	"gap": null,
+	"magType": "ml",
+	"type": "earthquake",
+	"title": "M 2.5 - 84km E of Old Iliamna, Alaska"
+	},
+	"geometry": {
+	"type": "Point",
+	"coordinates": [
+	-0.137626,
+	51.505617,  
+	110
+	]
+	},
+	"id": "ak12501966"
+	},
+	
+
+
+	
+	],
+	"bbox": [
+	-179.336,
+	-60.1862,
+	0,
+	179.8876,
+	71.6767,
+	306.15
+	]
+	}
+	)
--- js/script.js
+++ js/script.js
@@ -1,33 +1,37 @@
+$(document).ready(function() {
 
-function map() {}
+  function map() {}
 
-      var map;
+    var map;
 
-      function initialize() {
-        map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 13,
-          center: new google.maps.LatLng(51.508433,-0.127974),
-          mapTypeId: google.maps.MapTypeId.TERRAIN
+    function initialize() {
+      map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 13,
+        center: new google.maps.LatLng(51.508433,-0.127974),
+        mapTypeId: google.maps.MapTypeId.TERRAIN
+      });
+
+      // Create a <script> tag and set the USGS URL as the source.
+      var script = document.createElement('script');
+      // (In this example we use a locally stored copy instead.)
+      // script.src = 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp';
+      script.src = 'js/map.js';
+      document.getElementsByTagName('head')[0].appendChild(script);
+    }
+
+    // Loop through the results array and place a marker for each
+    // set of coordinates.
+    window.eqfeed_callback = function(results) {
+      for (var i = 0; i < results.features.length; i++) {
+        var coords = results.features[i].geometry.coordinates;
+        var latLng = new google.maps.LatLng(coords[1],coords[0]);
+        var marker = new google.maps.Marker({
+          position: latLng,
+          map: map
         });
-
-        // Create a <script> tag and set the USGS URL as the source.
-        var script = document.createElement('script');
-        // (In this example we use a locally stored copy instead.)
-        // script.src = 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp';
-        script.src = 'js/pints_taverns_GeoJSONP.js';
-        document.getElementsByTagName('head')[0].appendChild(script);
       }
+    }
 
-      // Loop through the results array and place a marker for each
-      // set of coordinates.
-      window.eqfeed_callback = function(results) {
-        for (var i = 0; i < results.features.length; i++) {
-          var coords = results.features[i].geometry.coordinates;
-          var latLng = new google.maps.LatLng(coords[1],coords[0]);
-          var marker = new google.maps.Marker({
-            position: latLng,
-            map: map
-          });
-        }
-      }
-      google.maps.event.addDomListener(window, 'load', initialize)
+    google.maps.event.addDomListener(window, 'load', initialize)
+    
+});
