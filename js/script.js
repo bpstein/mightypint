36d5f6c6d2adc157fb9b8c68aa05389747d3a956
Ben Stein <bumstain@gmail.com>
2016-01-27 00:48:53 +0000

    Customised map styling and map configuration

--- js/script.js
+++ js/script.js
@@ -1,37 +1,150 @@
-$(document).ready(function() {
+// Define the map styles
+var map_styles = [ 
+  {
+    "featureType": "administrative",
+    "stylers": [
+      { "visibility": "off" }
+    ]
+  },{
+    "featureType": "poi",
+    "stylers": [
+      { "visibility": "off" }
+    ]
+  },{
+    "featureType": "transit",
+    "stylers": [
+      { "visibility": "off" }
+    ]
+  },{
+    "featureType": "road",
+    "stylers": [
+      { "visibility": "off" }
+    ]
+  },{
+    "featureType": "landscape",
+    "stylers": [
+      { "color": "#FFE200" }
+    ]
+  },{
+    "featureType": "water",
+    "stylers": [
+      { "visibility": "on" },
+      { "color": "#4f92c6" }
+    ]
+  }
+];
 
-  function map() {}
+// Define the map styles when the map is zoomed
+var map_styles_zoomed = [
+  {
+    "featureType": "administrative",
+    "stylers": [
+      { "visibility": "on" }
+    ]
+  },{
+    "featureType": "poi",
+    "stylers": [
+      { "visibility": "on" }
+    ]
+  },{
+    "featureType": "transit",
+    "stylers": [
+      { "visibility": "on" }
+    ]
+  },{
+    "featureType": "road",
+    "stylers": [
+      { "visibility": "on" }
+    ]
+  },{
+    "featureType": "landscape",
+    "stylers": [
+      { "color": "#FFE200" }
+    ]
+  },{
+    "featureType": "water",
+    "stylers": [
+      { "visibility": "on" },
+      { "color": "#4f92c6" }
+    ]
+  },   {
+    "featureType": "poi.park",
+  "elementType": "geometry",
+    "stylers": [
+      { "color": "#FFFF00" }
+    ]
+  }
+];
 
-    var map;
+// Use the feature data to create styles
+var custom_map = new google.maps.StyledMapType(map_styles, {name: "Map style"});
+var custom_map_zoomed = new google.maps.StyledMapType(map_styles_zoomed, {name: "Map style zoomed"});
 
-    function initialize() {
-      map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 13,
-        center: new google.maps.LatLng(51.508433,-0.127974),
-        mapTypeId: google.maps.MapTypeId.TERRAIN
-      });
 
-      // Create a <script> tag and set the USGS URL as the source.
-      var script = document.createElement('script');
-      // (In this example we use a locally stored copy instead.)
-      // script.src = 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp';
-      script.src = 'js/map.js';
-      document.getElementsByTagName('head')[0].appendChild(script);
-    }
+// Map configuration variables
+var GOTMapCenter = new google.maps.LatLng(51.508433,-0.127974);
+var GOTMapZoom = 13;
+var GOTMapZoomMax = 15;
+var GOTMapZoomMin = 10;
 
-    // Loop through the results array and place a marker for each
-    // set of coordinates.
-    window.eqfeed_callback = function(results) {
-      for (var i = 0; i < results.features.length; i++) {
-        var coords = results.features[i].geometry.coordinates;
-        var latLng = new google.maps.LatLng(coords[1],coords[0]);
-        var marker = new google.maps.Marker({
-          position: latLng,
-          map: map
-        });
+// Map setup configuration
+var GOTMapOptions = { 
+      center: GOTMapCenter, 
+          zoom: GOTMapZoom,
+      maxZoom:GOTMapZoomMax,
+      minZoom:GOTMapZoomMin,
+      panControl: false,
+      mapTypeControl: false,
+       mapTypeControlOptions: {
+        mapTypeIds: [ 'map_styles', 'map_styles_zoomed']
+       }
+};
+
+// Map variable (the map itself)
+var GOTMap;
+
+// Create control panel (ie. site title and 2 buttons) 
+var controlPanelDiv = document.createElement('div');
+
+// Variable containing the style for the pop-up infobox.
+var pop_up_info = "border: 0px solid black; background-color: #ffffff; padding:15px; margin-top: 8px; border-radius:10px; -moz-border-radius: 10px; -webkit-border-radius: 10px; box-shadow: 1px 1px #888;";
+
+
+// When the page loads, the line below calls the function below called 'loadGOTMap' to load up the map
+google.maps.event.addDomListener(window, 'load', loadGOTMap);
+
+
+// Call this function when the page loads
+function loadGOTMap() {
+  
+// The line below creates the map, assigning it to this variable. 
+// The line below also loads the map into the div with the id 'festival-map' (see code within the 'body' tags below), and applies the 'GOTMapOptions' (above) to configure this map. 
+GOTMap = new google.maps.Map(document.getElementById("map"), GOTMapOptions); 
+
+//Assigning the two map styles defined above to the map.
+GOTMap.mapTypes.set('map_styles', custom_map);
+GOTMap.mapTypes.set('map_styles_zoomed', custom_map_zoomed);
+//Setting the one of the styles to display as default as the map loads.
+GOTMap.setMapTypeId('map_styles');
+
+
+//Continuously listens out for when the zoom level changes. This includs when the map zooms when a marker is clicked.
+google.maps.event.addListener(GOTMap, "zoom_changed", function() {
+  var newZoom = GOTMap.getZoom();
+  //If the map is zoomed in, the switch to the style that shows the higher level of detail.
+  if (newZoom > 5){
+    GOTMap.setMapTypeId('map_styles_zoomed');
       }
-    }
+  //Otherwise the map must be zoomed out, so use the style with the lower level of detail.
+  else {
+    GOTMap.setMapTypeId('map_styles');
+  }
 
-    google.maps.event.addDomListener(window, 'load', initialize)
-    
 });
+
+
+//Calls the function below to load up all the map markers.
+loadMapMarkers();
+
+}
+
