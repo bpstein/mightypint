1bdb72aa43448b0e6d24aca9996e852e39cbf3a2
Ben Stein <bumstain@gmail.com>
2016-01-29 00:06:52 +0000

    Added Arryn, Baratheon and Mormont sites

--- js/script.js
+++ js/script.js
@@ -231,5 +231,249 @@ function loadMarkers() {
         GOTMap.setZoom(5);
     }
   }
+
+  // THE THIRSTY BEAR, BLACKFRIARS
+
+    // MARKER FUNCTIONALITY
+    // Set the position of the map marker
+    var markerPositionThirstyBearBlackfriars = new google.maps.LatLng(51.507215, -0.108110);
+
+    // Set the icon to be used with the map marker
+    var markerIconThirstyBearBlackfriars = {
+      url: 'img/mormont.png',
+      // The size image file.
+      size: new google.maps.Size(500, 500),
+      //The point on the image to measure the anchor from. 0, 0 is the top left.
+      origin: new google.maps.Point(0, 0),
+      //The x y coordinates of the anchor point on the marker. e.g. If your map marker was a drawing pin then the anchor would be the tip of the pin.
+      anchor: new google.maps.Point(20, 24)
+    };
+
+    // Shape of the map marker
+    var markerShapeThirstyBearBlackfriars = {
+      coord: [12,4,216,22,212,74,157,70,184,111,125,67,6,56],
+      type: 'poly'
+    };
+
+    // Add the map marker to the map
+    markerThirstyBearBlackfriars = new google.maps.Marker({
+      position: markerPositionThirstyBearBlackfriars,
+      map: GOTMap,
+      title: 'The Thirsty Bear',
+      icon: markerIconThirstyBearBlackfriars,
+      shape: markerShapeThirstyBearBlackfriars,
+      zIndex: 102
+    })
+
+    // Reset z-indexes back to original values
+    function resetZindexes() {
+      markerThirstyBearBlackfriars(102);
+    }
+
+    // POPUP BOX FUNCTIONALITY
+    // Create the popup box
+    var infoBoxTextThirstyBearBlackfriars= document.createElement('div');
+    infoBoxTextThirstyBearBlackfriars.style.cssText = pop_up_info;
+    infoBoxTextThirstyBearBlackfriars.innerHTML = '<span class="pop_up_box_text" width="400" height="285" border="0">Here is some sample text.</span>';
+
+    // Popup configuration options of the popup box
+    var infoBoxOptionsThirstyBearBlackfriars = {
+      content: infoBoxTextThirstyBearBlackfriars,
+      disableAutoPan: false,
+      maxWidth: 100,
+      maxHeight: 400,
+      pixelOffset: new google.maps.Size(-241, 0),
+      zIndex: null,
+      boxStyle: { 
+        // background: "url('img/mormont.png') no-repeat",
+        opacity: 1,
+        width: "430px"
+      }, 
+      closeBoxMargin: "10px 2px 2px 2px",
+      closeBoxURL: "img/button_close.png",
+      infoBoxClearance: new google.maps.Size(1, 1),
+      isHidden: false,
+      pane: "floatPane",
+      enableEventPropagation: false
+    };
+
+    // Use the configuration options above to create a popup box
+    infoBoxThirstyBearBlackfriars = new InfoBox(infoBoxOptionsThirstyBearBlackfriars);
+
+    // Click event listener
+    google.maps.event.addListener(markerThirstyBearBlackfriars, 'click', function(){
+      infoBoxThirstyBearBlackfriars.open(GOTMap, this);
+      this.setZIndex(google.maps.Marker.MAX_ZINDEX + 1);
+      setZoomWhenMarkerClicked();
+      GOTMap.setCenter(markerThirstyBearBlackfriars.getPosition());
+    });
+
+  // THE THREE STAGS, KENNINGTON
+
+    // MARKER FUNCTIONALITY
+    // Set the position of the map marker
+    var markerPositionThreeStagsKennington = new google.maps.LatLng(51.496888, -0.111306);
+
+    // Set the icon to be used with the map marker
+    var markerIconThreeStagsKennington = {
+      url: 'img/baratheon.png',
+      // The size image file.
+      size: new google.maps.Size(500, 500),
+      //The point on the image to measure the anchor from. 0, 0 is the top left.
+      origin: new google.maps.Point(0, 0),
+      //The x y coordinates of the anchor point on the marker. e.g. If your map marker was a drawing pin then the anchor would be the tip of the pin.
+      anchor: new google.maps.Point(20, 24)
+    };
+
+    // Shape of the map marker
+    var markerShapeThreeStagsKennington = {
+      coord: [12,4,216,22,212,74,157,70,184,111,125,67,6,56],
+      type: 'poly'
+    };
+
+    // Add the map marker to the map
+    markerThreeStagsKennington = new google.maps.Marker({
+      position: markerPositionThreeStagsKennington,
+      map: GOTMap,
+      title: 'The Three Stags',
+      icon: markerIconThreeStagsKennington,
+      shape: markerShapeThreeStagsKennington,
+      zIndex: 102
+    })
+
+    // Reset z-indexes back to original values
+    function resetZindexes() {
+      markerThreeStagsKennington(103);
+    }
+
+    // POPUP BOX FUNCTIONALITY
+    // Create the popup box
+    var infoBoxTextThreeStagsKennington = document.createElement('div');
+    infoBoxTextThreeStagsKennington.style.cssText = pop_up_info;
+    infoBoxTextThreeStagsKennington.innerHTML = '<span class="pop_up_box_text" width="400" height="285" border="0">Here is some sample text.</span>';
+
+    // Popup configuration options of the popup box
+    var infoBoxOptionsThreeStagsKennington = {
+      content: infoBoxTextThreeStagsKennington,
+      disableAutoPan: false,
+      maxWidth: 100,
+      maxHeight: 400,
+      pixelOffset: new google.maps.Size(-241, 0),
+      zIndex: null,
+      boxStyle: { 
+        // background: "url('img/baratheon.png') no-repeat",
+        opacity: 1,
+        width: "430px"
+      }, 
+      closeBoxMargin: "10px 2px 2px 2px",
+      closeBoxURL: "img/button_close.png",
+      infoBoxClearance: new google.maps.Size(1, 1),
+      isHidden: false,
+      pane: "floatPane",
+      enableEventPropagation: false
+    };
+
+    // Use the configuration options above to create a popup box
+    infoBoxThreeStagsKennington = new InfoBox(infoBoxOptionsThreeStagsKennington);
+
+    // Click event listener
+    google.maps.event.addListener(markerThreeStagsKennington, 'click', function(){
+      infoBoxThreeStagsKennington.open(GOTMap, this);
+      this.setZIndex(google.maps.Marker.MAX_ZINDEX + 1);
+      setZoomWhenMarkerClicked();
+      GOTMap.setCenter(markerThreeStagsKennington.getPosition());
+    });
+
+    // Zoom in the map when a marker is clicked
+    function setZoomWhenMarkerClicked() {
+      var currentZoom = GOTMap.getZoom();
+      if (currentZoom < 5) {
+        GOTMap.setZoom(5);
+    }
+  }
+
+  // THE FALCON, CLAPHAM
+
+    // MARKER FUNCTIONALITY
+    // Set the position of the map marker
+    var markerPositionTheFalconClapham = new google.maps.LatLng(51.464486, -0.129030);
+
+    // Set the icon to be used with the map marker
+    var markerIconTheFalconClapham = {
+      url: 'img/arryn.png',
+      // The size image file.
+      size: new google.maps.Size(500, 500),
+      //The point on the image to measure the anchor from. 0, 0 is the top left.
+      origin: new google.maps.Point(0, 0),
+      //The x y coordinates of the anchor point on the marker. e.g. If your map marker was a drawing pin then the anchor would be the tip of the pin.
+      anchor: new google.maps.Point(20, 24)
+    };
+
+    // Shape of the map marker
+    var markerShapeTheFalconClapham = {
+      coord: [12,4,216,22,212,74,157,70,184,111,125,67,6,56],
+      type: 'poly'
+    };
+
+    // Add the map marker to the map
+    markerTheFalconClapham = new google.maps.Marker({
+      position: markerPositionTheFalconClapham,
+      map: GOTMap,
+      title: 'The Three Stags',
+      icon: markerIconTheFalconClapham,
+      shape: markerShapeTheFalconClapham,
+      zIndex: 102
+    })
+
+    // Reset z-indexes back to original values
+    function resetZindexes() {
+      markerTheFalconClapham(103);
+    }
+
+    // POPUP BOX FUNCTIONALITY
+    // Create the popup box
+    var infoBoxTextTheFalconClapham = document.createElement('div');
+    infoBoxTextTheFalconClapham.style.cssText = pop_up_info;
+    infoBoxTextTheFalconClapham.innerHTML = '<span class="pop_up_box_text" width="400" height="285" border="0">Here is some sample text.</span>';
+
+    // Popup configuration options of the popup box
+    var infoBoxOptionsTheFalconClapham = {
+      content: infoBoxTextTheFalconClapham,
+      disableAutoPan: false,
+      maxWidth: 100,
+      maxHeight: 400,
+      pixelOffset: new google.maps.Size(-241, 0),
+      zIndex: null,
+      boxStyle: { 
+        // background: "url('img/baratheon.png') no-repeat",
+        opacity: 1,
+        width: "430px"
+      }, 
+      closeBoxMargin: "10px 2px 2px 2px",
+      closeBoxURL: "img/button_close.png",
+      infoBoxClearance: new google.maps.Size(1, 1),
+      isHidden: false,
+      pane: "floatPane",
+      enableEventPropagation: false
+    };
+
+    // Use the configuration options above to create a popup box
+    infoBoxTheFalconClapham = new InfoBox(infoBoxOptionsTheFalconClapham);
+
+    // Click event listener
+    google.maps.event.addListener(markerTheFalconClapham, 'click', function(){
+      infoBoxTheFalconClapham.open(GOTMap, this);
+      this.setZIndex(google.maps.Marker.MAX_ZINDEX + 1);
+      setZoomWhenMarkerClicked();
+      GOTMap.setCenter(markerTheFalconClapham.getPosition());
+    });
+
+    // Zoom in the map when a marker is clicked
+    function setZoomWhenMarkerClicked() {
+      var currentZoom = GOTMap.getZoom();
+      if (currentZoom < 5) {
+        GOTMap.setZoom(5);
+    }
+  }
 }
 
