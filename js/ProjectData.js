5bfeeee6c45e04dce61f1c33d2551e0881e51906
Ben Stein <bumstain@gmail.com>
2016-03-08 20:38:25 +0000

    Populated initial JSON data for venues

---
+++ js/ProjectData.js
@@ -0,0 +1,105 @@
+var venues = {
+	"venues": [{
+			"name": "The Red Lion",
+			"house": "Lannister",
+			"words": "",
+			"details": "This is a Lannister pub.",
+			"image": [""],
+      "address": "",
+			"url": "",	
+			"lat": "", 
+			"lng": ""
+		}, {
+			"name": "The Hand and Flower",
+			"house": "Allyrion",
+			"words": "No Foe May Pass",
+			"details": "",
+			"image": [""],
+      "address": "",
+			"url": "",
+			"lat": "", 
+			"lng": ""
+		}
+			"name": "",
+			"house": "Ambrose",
+			"words": "Never Resting",
+			"details": "",
+			"image": [""],
+      "address": "",
+			"url": "",
+			"lat": "", 
+			"lng": ""
+		}, {
+			"name": "The Falcon",
+			"house": "Arryn",
+			"words": "As High as Honor",
+			"details": "",
+			"image": [""],
+      "address": "",
+			"url": "",
+			"lat": "", 
+			"lng": ""
+		}, {
+			"name": "The Sun",
+			"house": "Ashford",
+			"words": "Our Sun Shines Bright",
+			"details": "",
+			"image": [""],
+      "address": "",
+			"url": "",
+			"lat": "", 
+			"lng": ""
+		}, {
+			"name": "The Three Stags",
+			"house": "Baratheon",
+			"words": "Ours Is The Fury",
+			"details": "",
+			"image": [""],
+      "address": "",
+			"url": "",
+			"lat": "", 
+			"lng": ""
+		}, {
+			"name": "Golden Bee",
+			"house": "Beesbury",
+			"words": "Beware Our Sting",
+			"details": "",
+			"image": [""],
+      "address": "",
+			"url": "",
+			"lat": "", 
+			"lng": ""
+		}, {
+			"name": "The Thirsty Bear",
+			"house": "Mormont",
+			"words": "Here We Stand",
+			"details": "",
+			"image": [""],
+      "address": "",
+			"url": "",
+			"lat": "", 
+			"lng": ""
+		}, {
+			"name": "Rising Sun",
+			"house": "Martell",
+			"words": "Unbowed, Unbent, Unbroken",
+			"details": "",
+			"image": [""],
+      "address": "",
+			"url": "",
+			"lat": "", 
+			"lng": ""
+		}, {
+			"name": "The Seahorse",
+			"house": "Velaryon",
+			"words": "The Old, The True, The Brave",
+			"details": "",
+			"image": [""],
+      "address": "",
+			"url": "",
+			"lat": "", 
+			"lng": ""
+		}, {
+
+	]
+};
